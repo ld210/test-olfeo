e53aacf61459192267614ca069a0f41507a0fce4
scalpaine <ybahou.pro@gmail.com>
2021-04-30 10:33:58 +0200

    chore: add formular test units

--- src/app/formular/formular.component.spec.ts
+++ src/app/formular/formular.component.spec.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { FormBuilder } from "@angular/forms";
 
@@ -24,5 +28,43 @@ describe("FormularComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should contain required inputs", () => {
+    fixture = TestBed.createComponent(FormularComponent);
+    fixture.detectChanges();
+    const render = fixture.debugElement.nativeElement;
+
+    expect(render.querySelector("input[formControlName='lastName']")).not.toBe(
+      null
+    );
+    expect(render.querySelector("input[formControlName='firstName']")).not.toBe(
+      null
+    );
+    expect(render.querySelector("input[formControlName='email']")).not.toBe(
+      null
+    );
+    expect(render.querySelector("input[formControlName='password']")).not.toBe(
+      null
+    );
+    expect(
+      render.querySelector("input[formControlName='passwordConfirm']")
+    ).not.toBe(null);
+    expect(render.querySelector("div[formArrayName='addresses']")).not.toBe(
+      null
+    );
+  });
+
+  it("submit button should call submit function", () => {
+    fixture = TestBed.createComponent(FormularComponent);
+    fixture.detectChanges();
+    const button = fixture.debugElement.nativeElement.querySelector(
+      "button[type='submit']"
+    );
+    button.click();
+
+    fixture
+      .whenStable()
+      .then(() => expect(component.onSubmit).toHaveBeenCalledWith());
   });
 });
